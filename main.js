3b903a5c033c3fc3adf822217cdd11365ebab5be
Luca Bonini <luca14bonini@gmail.com>
2023-08-01 20:44:31 -0500

    changes

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
 console.log("H.P.");
+
+const students = [
+    {
+      id: 0,
+      name: "Hermione Granger",
+      house: "Gryffondor",
+    },
+  
+    {
+      id: 1,
+      name: "Cedric Diggory",
+      house: "Hufflepuff",
+    },
+  
+    {
+      id: 2,
+      name: "Luna Lovegood",
+      house: "Ravenclaw",
+    },
+  ];
+  const studentsDeleted = []
+
+
+const renderToDom = (divId, htmlToRender) => {
+  const selectedDiv = document.querySelector(divId);
+  selectedDiv.innerHTML = htmlToRender;
+};
+
+const filter = (array, houseString) => {
+  const houseArray = [];
+
+  for (const student of array) {
+    if (student.house === houseString) {
+       houseArray.push(student);
+    }
+  }
+
+  return houseArray;
+}
